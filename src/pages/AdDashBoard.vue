c9f779db49a4bc34ea401549fb278d26f08fb96d
pympimp <dumped.112@gmail.com>
2023-12-23 11:38:25 +0700

    dashboard

--- src/pages/AdDashBoard.vue
+++ src/pages/AdDashBoard.vue
@@ -1,25 +1,161 @@
+<template>
+  <div>
+    <AdNav />
+    <div class="container">
+      <div class="buttons">
+        <h1>เมนู</h1>
+
+        <router-link to="/AdminManageArticle" class="button">
+          <button
+            @click="scrollToAdditionalBox('html')"
+            class="scroll-button"
+            :style="{ backgroundColor: currentContent === 'html' ? '#EE5684' : '#1F1F1F' }"
+          >
+            <img src="/images/html.png" alt="Image" class="button-image" />
+            <p>HTML</p>
+          </button>
+        </router-link>
+
+        <div class="button">
+          <button
+            @click="scrollToAdditionalBox('css')"
+            class="scroll-button"
+            :style="{ backgroundColor: currentContent === 'css' ? '#EE5684' : '#1F1F1F' }"
+          >
+            <img src="/images/css.png" alt="Image" class="button-image" />
+            <p>CSS</p>
+          </button>
+        </div>
+
+        <div class="button">
+          <button
+            @click="scrollToAdditionalBox('javascript')"
+            class="scroll-button"
+            :style="{ backgroundColor: currentContent === 'javascript' ? '#EE5684' : '#1F1F1F' }"
+          >
+            <img src="/images/javascript.png" alt="Image" class="button-image" />
+            <p>JavaScript</p>
+          </button>
+        </div>
+      </div>
+
+      <!-- <BoxArticie v-if="currentContent && languageData[currentContent]" :contentData="languageData[currentContent]" /> -->
+      <BoxArticie v-if="currentContent === 'html'" :contentData="languageData.html" />
+      <BoxArticie v-if="currentContent === 'css'" :contentData="languageData.css" />
+      <BoxArticie v-if="currentContent === 'javascript'" :contentData="languageData.javascript" />
+      <!-- <div v-else class="container-no-data">
+        <h1>No Data</h1>
+      </div> -->
+      <p>Test</p>
+    </div>
+  </div>
+</template>
+
 <script>
 import AdNav from '@/components/AdNav.vue'
-// import 'bootstrap-icons/font/bootstrap-icons.css'
+import BoxArticie from '@/components/BoxArticie.vue'
 
 export default {
   components: {
     AdNav,
+    BoxArticie
+  },
+  data() {
+    return {
+      currentContent: 'html', // กำหนดให้แสดงข้อมูล HTML เริ่มต้น
+      languageData: {
+        html: {
+          
+        },
+
+        css: {
+          
+        },
+
+        javascript: {
+
+        }
+      }
+    }
   },
 
+  methods: {
+    scrollToAdditionalBox(contentId) {
+      this.currentContent = contentId
+    }
+  }
 }
 </script>
 
-<template>
-
-    <AdNav />
-    
-</template>
-
 <style scoped>
-body {
-    background-color: #3498db; /* เปลี่ยนเป็นสีฟ้า */
+h1 {
+  margin-top: 50px;
+  margin-left: 35px;
+  margin-bottom: 2%;
+  color: #fffdfd;
+  font-size: 27px;
+  font-weight: bolder;
 }
 
+.button-image {
+  transform: translateY(10%);
+  width: 40px;
+  height: 40px;
+  margin-left: 65px;
+  /* background-color: aqua; */
+}
 
+.scroll-button {
+  margin-top: 5px;
+  margin-left: -40px;
+  margin-bottom: 15px;
+  width: 270px;
+  height: 50px;
+  background-color: #1f1f1f;
+  /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);  */
+  border-radius: 50px;
+  cursor: pointer;
+}
+
+.scroll-button:hover {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  background-color: #ee5684;
+}
+
+p {
+  font-size: 20px;
+  margin-left: 115px;
+  transform: translateY(-105%);
+  font-weight: bolder;
+  color: #fffdfd;
+  width: 20px;
+  /* background-color: aqua; */
+}
+
+.container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+}
+
+/* หากต้องการให้เปิดมาแล้วเป็นกล่องเปล่าๆ */
+.container-no-data {
+  background-color: #f5f5f5;
+  margin-top: 50px;
+  margin-right: 50px;
+  width: 1000px;
+  height: 1000px;
+}
+.container-no-data h1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #000;
+}
+
+.buttons {
+  grid-column: 1 / 2;
+}
 </style>
+
+
--- src/pages/AdManageArticle.vue
+++ src/pages/AdManageArticle.vue
@@ -1,38 +1,32 @@
 <template>
+  <!-- Add this line to your HTML file -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@{latest_version}/font/bootstrap-icons.css" rel="stylesheet">
+
     <div class="w-screen h-screen flex">
       <!-- Side bar -->
       <div class="w-[400px] h-full bg-gray-200 text-white" v-show="showSide">
         <div class="h-[50px] bg-gray-900 flex justify-start  items-center ">
           <div class="px-[20px]">
-            <h3 class="font-bold text-xl">Admin Dashboard</h3>
+            <h3 class="font-bold text-xl">Admin</h3>
           </div>
         </div>
         <div class="h-[calc(100vh-50px)] bg-gray-800 py-[20px]">
           <div class="flex flex-col justify-between h-full px-[20px] space-y-[10px]">
             <div class=" flex flex-col justify-between space-y-[10px]">
               <router-link to="/AdminDashboard" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800  transition duration-400 ease-in-out">
-                <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
-                </svg>
+                <i class="fa-regular fa-image"></i>
                 Dashboard
               </router-link>
-              <router-link to="/profile" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
-                <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
-                </svg>
-                Profile
+              <router-link to="" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
+                User
               </router-link>
-              <router-link to="/message" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
-                <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v7h-2l-1 2H8l-1-2H5V5z" clip-rule="evenodd"></path>
-                </svg>
-                Messages
+              <router-link to="" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
+                
+                Article
               </router-link>
-              <router-link to="/download" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md rounded-b-lg hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
-                <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M2 9.5A3.5 3.5 0 005.5 13H9v2.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 15.586V13h2.5a4.5 4.5 0 10-.616-8.958 4.002 4.002 0 10-7.753 1.977A3.5 3.5 0 002 9.5zm9 3.5H9V8a1 1 0 012 0v5z" clip-rule="evenodd"></path>
-                </svg>
-                Download
+              <router-link to="" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md rounded-b-lg hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
+                
+                Class
               </router-link>
   
             </div>
@@ -54,7 +48,7 @@
           <!-- Hambuger menu -->
           <div class="cursor-pointer w-[30px]" @click="toggleSideBar">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class=" w-[25px] h-[25px]">
-              <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+      
               <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
             </svg>
           </div>
@@ -102,7 +96,7 @@
         </div>
         <div class="h-[calc(100vh-50px)] bg-gray-50 p-[20px]">
           <div class="border border-gray-300 rounded-md p-[20px] h-full">
-            <router-view></router-view>
+            <h1>Dashboard</h1>
           </div>
         </div>
       </div>
@@ -110,6 +104,9 @@
     </div>
   </template>
   <script>
+  // import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+
   export default {
     data() {
       return {
@@ -128,12 +125,19 @@
         this.showDropDown = !this.showDropDown
   
       }
+    },
+    components: {
+      // FontAwesomeIcon,
     }
   
   }
   </script>
   
   <style>
+  h1 {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
     .admin{
         background-color: rgb(183, 229, 229);
     }
