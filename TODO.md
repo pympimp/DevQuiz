fee027023a77d308b4913d4c0821226eb0bd8675
SoCuteLyz <130419425+SoCuteLyz@users.noreply.github.com>
2024-01-23 23:06:37 +0700

    mit

--- TODO.md
+++ TODO.md
@@ -6,6 +6,7 @@ Project Description
 
 ### Todo
 
+- [ ] login stat dashborad  
 
 ### In Progress
 
--- src/pages/AdDashBoard.vue
+++ src/pages/AdDashBoard.vue
@@ -1,38 +1,57 @@
-<template>    <div class="h-[calc(97vh-50px)] bg-gray-50 p-[20px]" style="background-color: #F5FCFF;">
-  <h1>Dashboard</h1>
-  <div class="border border-gray-300 rounded-md p-[20px] h-full">
-    <!-- หัวข้อ -->
-    <div class="containers">
+<template>
+  <div class="h-[calc(97vh-50px)] bg-gray-50 p-[20px]" style="background-color: #f5fcff">
+    <h1>Dashboard</h1>
+    <div class="border border-gray-300 rounded-md p-[20px] h-full">
+      <!-- หัวข้อ -->
+      <div class="containers">
         <div class="box-container">
           <div class="box-1">
-
             <div class="about-text">
-                  <h1>ยินดีต้อนรับ</h1>
-                  <p>ระบบ e-Laerning แบบ Interactive สำหรับวิชาการเขียนโปรแกรมเบื้องต้น</p>
-                </div>
-              <div class="about-content">
-                <div class="about-image">
-                  <img src="/images/main.png" alt="Main Image" class="h-auto">
-                </div>
-                
+              <h1>ยินดีต้อนรับ</h1>
+              <p>ระบบ e-Laerning แบบ Interactive สำหรับวิชาการเขียนโปรแกรมเบื้องต้น</p>
+            </div>
+            <div class="about-content">
+              <div class="about-image">
+                <img src="/images/main.png" alt="Main Image" class="h-auto" />
               </div>
             </div>
+          </div>
           <div class="box-2-3-container">
-            <div class="box-2">จำนวนผู้ใช้งานวันนี้
-              <p>40000 คน</p>
+            <div class="box-2">
+              จำนวนผู้ใช้งานวันนี้
+              <p>0 คน</p>
             </div>
-            <div class="box-3">จำนวนสมาชิกที่เข้าใช้ระบบ
-              <p>400000000000000000 คน</p>
+            <div class="box-3">
+              จำนวนสมาชิกที่เข้าใช้ระบบ
+              <p v-if="userStatAll">{{ userStatAll.totalCount }} คน</p>
             </div>
           </div>
           <!-- <div class="box-4">Box 4</div> -->
         </div>
       </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
+import { useAuthenStore } from '../stores/auth'
+
+const authenStore = useAuthenStore()
+const userStatAll = ref()
+
+onMounted(() => {
+  setTimeout(() => {
+    const eventSource = new EventSource(
+      `http://localhost:5000/test-elearning-b0646/us-central1/api/user/userStat/${authenStore.auth.id}`
+    )
+    eventSource.addEventListener('message', (event) => {
+      const eventData = JSON.parse(event.data)
+      userStatAll.value = eventData
+      console.log('stat', eventData)
+    })
+  }, 800)
+})
 </script>
 
 <style scoped>
@@ -56,13 +75,12 @@
   width: 400px;
   height: 250px;
   margin-top: -45px;
-   /* background-color: #bfffbf; */
-   margin-right: 30px;
+  /* background-color: #bfffbf; */
+  margin-right: 30px;
 }
 
 .about-text {
- margin-top: 55px;
-  
+  margin-top: 55px;
 }
 
 .about-text p {
