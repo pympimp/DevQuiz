cf6679590faca9ac4c76b0604a62b93e58f36ab7
gib0953460152 <ggnatthawadee9@gmail.com>
2024-02-01 12:54:54 +0700

    2

--- src/pages/AdDashBoard.vue
+++ src/pages/AdDashBoard.vue
@@ -1,18 +1,17 @@
 <template>
   <half-circle-spinner
-    :animation-duration="1000"
-    :size="60"
-    color="#ff1d5e"
-    class="loading"
-    v-if="isLoading"
-  />
+            :animation-duration="1000"
+            :size="60"
+            color="#ff1d5e"
+            class="loading"
+            v-if="isLoading"
+          />
   <div v-if="!isLoading" class="h-[calc(100vh-50px)] p-[20px]" style="background-color: #fff">
     <h1>Dashboard</h1>
     <div class="containers">
       <div class="box-container">
         <div class="box-1">
-          <apexchart width="500" type="bar" :options="chartOptions" :series="series"></apexchart>
-          <apexchart width="380" type="donut" :options="Options" :series="series1"></apexchart>
+          <VueApexCharts type="bar" :options="chartOptions" :series="series" style="width: 500px;height: 300px;"></VueApexCharts>
           <!-- <div class="about-text">
                   <h1>ยินดีต้อนรับ</h1>
                   <p>ระบบ e-Laerning แบบ Interactive สำหรับวิชาการเขียนโปรแกรมเบื้องต้น</p>
@@ -28,12 +27,13 @@
         <div class="box-2">
           จำนวนผู้ใช้งานวันนี้
           <transition mode="out-in">
-            <p v-if="userStatAll" :key="userStatAll.totalCount">{{ userStatAll.totalCount }} คน</p>
-          </transition>
+          <p v-if="userStatAll" :key="userStatAll.totalCount">{{ userStatAll.totalCount }} คน</p>
+        </transition>
+
         </div>
         <div class="box-3">
           จำนวนสมาชิกที่เข้าใช้ระบบ
-          <p v-if="userData">{{ userData.length }} คน</p>
+          <p v-if="userData" >{{ userData.length }} คน</p>
         </div>
       </div>
       <!-- <div class="box-4">Box 4</div> -->
@@ -43,34 +43,30 @@
 
 <script setup>
 import { onMounted, ref } from 'vue'
-import axios from 'axios'
-import { useAuthenStore } from '../stores/auth'
+import axios from 'axios';
+// import { useAuthenStore } from '../stores/auth'
 import { HalfCircleSpinner } from 'epic-spinners'
+import VueApexCharts from 'vue3-apexcharts'
 
 // const authenStore = useAuthenStore()
 const userStatAll = ref()
 const isLoading = ref(false)
 const userData = ref()
-
-const Options = ref({
-  labels: ['สัปดาห์ที่-1', 'สัปดาห์ที่-2', 'สัปดาห์ที่-3', 'สัปดาห์ที่-4']
-})
-const series1 = ref([44, 55, 41, 17])
 const chartOptions = ref({
   chart: {
-    id: 'basic-bar'
+    id: 'basic-bar',
   },
   xaxis: {
-    categories: ['สัปดาห์ที่-1', 'สัปดาห์ที่-2', 'สัปดาห์ที่-3', 'สัปดาห์ที่-4']
-  }
-})
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+  },
+});
 
 const series = ref([
   {
     name: 'series-1',
-    data: [30, 40, 45, 50]
-  }
-])
+    data: [30, 40, 45, 50, 49, 60, 70, 91],
+  },
+]);
 
 onMounted(() => {
   isLoading.value = true
@@ -81,19 +77,19 @@ onMounted(() => {
   }, 800)
 })
 
-const countStat = () => {
-  const fetchData = () => {
-    if (authenStore.auth.id) {
-      const eventSource = new EventSource(
-        `http://localhost:5000/test-elearning-b0646/us-central1/api/user/userStat/${authenStore.auth.id}`
-      )
-      eventSource.addEventListener('message', (event) => {
-        const eventData = JSON.parse(event.data)
-        userStatAll.value = eventData
-        console.log('stat', userStatAll.value)
-      })
-    }
-  }
+// const countStat = () => {
+//   const fetchData = () => {
+//     if (authenStore.auth.id) {
+//       const eventSource = new EventSource(
+//         `http://localhost:5000/test-elearning-b0646/us-central1/api/user/userStat/${authenStore.auth.id}`
+//       )
+//       eventSource.addEventListener('message', (event) => {
+//         const eventData = JSON.parse(event.data)
+//         userStatAll.value = eventData
+//         console.log("stat",userStatAll.value)
+//       })
+//     }
+//   }
 
 //   // เรียก fetchData ครั้งแรก
 //   fetchData()
@@ -102,12 +98,12 @@ const countStat = () => {
 //   setInterval(fetchData, 60000) // 1 นาที = 60,000 มิลลิวินาที
 // }
 
-const fetchUser = async () => {
-  const result = await axios.get('http://localhost:5000/test-elearning-b0646/us-central1/api/user')
-  if (result) {
-    userData.value = result.data
-    console.log(userData.value)
-  }
+const fetchUser = async() =>{
+const result = await axios.get("http://localhost:5000/test-elearning-b0646/us-central1/api/user")
+if(result){
+  userData.value = result.data
+  console.log(userData.value)
+}
 }
 </script>
 
@@ -131,10 +127,7 @@ h1 {
   border-radius: 5px;
   height: 350px;
   display: flex;
-  flex-direction: row;
   box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.5); /* เพิ่มเงาให้กับกล่อง */
-  justify-content: space-around;
-  align-items: center;
 }
 
 .about-content {
