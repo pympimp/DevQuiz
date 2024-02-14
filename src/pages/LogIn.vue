98876d9438444b253bb4a44310d6ce4bc5a93561
SoCuteLyz <130419425+SoCuteLyz@users.noreply.github.com>
2024-02-14 10:40:20 +0700

    fix

--- src/pages/LogIn.vue
+++ src/pages/LogIn.vue
@@ -63,7 +63,7 @@ export default {
     },
     async login() {
       try {
-        const result = await axios.post("http://localhost:3000/user/login",{
+        const result = await axios.post("https://172.16.49.36:3000/user/login",{
           username:this.username,
           password:this.password
         });
