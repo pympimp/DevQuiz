eb12563082071f8ee83c66c3d115e63be54620eb
SoCuteLyz <130419425+SoCuteLyz@users.noreply.github.com>
2024-02-14 09:35:45 +0700

    fix3

--- src/pages/LogIn.vue
+++ src/pages/LogIn.vue
@@ -63,7 +63,7 @@ export default {
     },
     async login() {
       try {
-        const result = await axios.post("https://devquiz-five.vercel.app/user/login",{
+        const result = await axios.post("https://localhost:3000/user/login",{
           username:this.username,
           password:this.password
         });
