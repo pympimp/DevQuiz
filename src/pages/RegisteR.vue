be99ee4fec19f1da396c5187abdaa0d759300174
SoCuteLyz <130419425+SoCuteLyz@users.noreply.github.com>
2024-02-15 01:12:40 +0700

    up

--- src/pages/RegisteR.vue
+++ src/pages/RegisteR.vue
@@ -65,7 +65,7 @@ export default {
     async signup(){
   try {
     // ส่วนของการส่งข้อมูล
-    const result = await axios.post("http://localhost:3000/user/register", {
+    const result = await axios.post("http://192.168.1.110:3000/user/register", {
       email: this.email,
       username: this.username,
       password: this.password,
